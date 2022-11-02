ed4b2a4843d4410f605c81676318086b2ceeba2b
Eatmeta <metagroul@gmail.com>
2022-11-02 18:48:38 +0300

    Implement a method that returns which of the opponents will have time to get to which cells of the labyrinth faster than others, if the players take turns one after another

--- RivalsTask.cs
+++ RivalsTask.cs
@@ -1,18 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Rivals
 {
-	public class RivalsTask
-	{
-		public static IEnumerable<OwnedLocation> AssignOwners(Map map)
-		{
-			yield break;
-		}
-	}
+    public class RivalsTask
+    {
+        private static readonly Point[] Offsets = {new Point(-1, 0), new Point(1, 0), new Point(0, -1), new Point(0, 1)};
+
+        public static IEnumerable<OwnedLocation> AssignOwners(Map map)
+        {
+            var visited = new HashSet<Point>();
+            var queue = new Queue<OwnedLocation>();
+
+            for (var i = 0; i < map.Players.Length; i++)
+            {
+                visited.Add(map.Players[i]);
+                var startOwnedLocation = new OwnedLocation(i, map.Players[i], 0);
+                queue.Enqueue(startOwnedLocation);
+                yield return startOwnedLocation;
+            }
+
+            while (queue.Count != 0)
+            {
+                var currentOwnedLocation = queue.Dequeue();
+                var nextPoints = GetPossibleNextPoint(map, visited, currentOwnedLocation);
+                if (nextPoints == null)
+                    continue;
+                foreach (var nextPoint in nextPoints)
+                {
+                    var nextOwnedLocation = new OwnedLocation(currentOwnedLocation.Owner, nextPoint,
+                        currentOwnedLocation.Distance + 1);
+                    queue.Enqueue(nextOwnedLocation);
+                    visited.Add(nextPoint);
+                    yield return nextOwnedLocation;
+                }
+            }
+        }
+
+        private static List<Point> GetPossibleNextPoint(Map map, HashSet<Point> visited,
+            OwnedLocation currentOwnedLocation)
+        {
+            var lastPoint = currentOwnedLocation.Location;
+            var nextPoints = Offsets
+                .Where(offset => !visited.Contains(new Point(offset.X + lastPoint.X, offset.Y + lastPoint.Y))
+                                 && map.InBounds(offset + (Size) lastPoint)
+                                 && map.Maze[(offset + (Size) lastPoint).X, (offset + (Size) lastPoint).Y] !=
+                                 MapCell.Wall)
+                .Select(p => p + (Size) lastPoint)
+                .ToList();
+            return nextPoints;
+        }
+    }
 }
